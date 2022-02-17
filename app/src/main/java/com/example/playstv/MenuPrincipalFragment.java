cfa8b3970c7585c7da9ce99073da263150c8e515
jbecerra <josemariabecerra30@gmail.com>
2022-02-17 20:03:51 +0100

    v1

--- app/src/main/java/com/example/playstv/MenuPrincipalFragment.java
+++ app/src/main/java/com/example/playstv/MenuPrincipalFragment.java
@@ -2,21 +2,41 @@ package com.example.playstv;
 
 import android.os.Bundle;
 
+import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import com.example.playstv.databinding.*;
+
+import com.google.android.material.progressindicator.BaseProgressIndicator;
 
 public class MenuPrincipalFragment extends Fragment {
     NavController navController;
 
+    private FragmentMenuPrincipalBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false);
         return inflater.inflate(R.layout.fragment_menu_principal, container, false);
+
     }
 }
