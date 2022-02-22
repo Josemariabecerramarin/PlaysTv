d020da0d680b91bae860c2d46ac3dfed208aae3d
jbm19 <josemariabecerra30@gmail.com>
2022-02-22 15:09:33 +0100

    v1

--- app/src/main/java/com/example/playstv/MenuPrincipalFragment.java
+++ app/src/main/java/com/example/playstv/MenuPrincipalFragment.java
@@ -1,42 +1,54 @@
 package com.example.playstv;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.playstv.databinding.*;
 
 import com.google.android.material.progressindicator.BaseProgressIndicator;
 
-public class MenuPrincipalFragment extends Fragment {
+public class MenuPrincipalFragment<ibLis> extends Fragment {
     NavController navController;
+    ImageButton buttonSuscripciones;
 
-    private FragmentMenuPrincipalBinding binding;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false);
         return inflater.inflate(R.layout.fragment_menu_principal, container, false);
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        buttonSuscripciones = view.findViewById(R.id.botonSuscripciones);
+
+
+        buttonSuscripciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_menuSuscripcionesFragment);
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/playstv/MenuSuscripcionesFragment.java
@@ -0,0 +1,26 @@
+package com.example.playstv;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MenuSuscripcionesFragment extends Fragment {
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_suscripciones, container, false);
+    }
+
+}
