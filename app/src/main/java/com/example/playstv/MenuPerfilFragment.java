1addc7e40dda2ec1bba92a0203f919dbe9929b89
jbm19 <josemariabecerra30@gmail.com>
2022-02-23 11:21:44 +0100

    v2.1

--- app/src/main/java/com/example/playstv/MenuPerfilFragment.java
+++ app/src/main/java/com/example/playstv/MenuPerfilFragment.java
@@ -1,76 +1,61 @@
 package com.example.playstv;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import android.renderscript.ScriptGroup;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.playstv.databinding.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-import com.google.android.material.progressindicator.BaseProgressIndicator;
 
-public class MenuPrincipalFragment<ibLis> extends Fragment {
+public class MenuPerfilFragment extends Fragment {
     NavController navController;
+    ImageButton buttonVolver;
     ImageButton buttonSuscripciones;
-    ImageButton buttonPerfil;
     ImageButton buttonCamara;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_menu_principal, container, false);
-
+        return inflater.inflate(R.layout.fragment_menu_perfil, container, false);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
 
+        buttonVolver = view.findViewById(R.id.logoVolver);
         buttonSuscripciones = view.findViewById(R.id.botonSuscripciones);
-        buttonPerfil = view.findViewById(R.id.botonPerfil);
         buttonCamara = view.findViewById(R.id.botonCamara);
 
 
-        buttonSuscripciones.setOnClickListener(new View.OnClickListener(){
+        buttonVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_menuSuscripcionesFragment);
+                navController.navigate(R.id.action_menuPrincipalFragment);
             }
         });
 
-        buttonPerfil.setOnClickListener(new View.OnClickListener() {
+        buttonSuscripciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            navController.navigate(R.id.action_menuPerfilFragment);
+                navController.navigate(R.id.action_menuPerfilFragment_to_menuSuscripcionesFragment);
             }
         });
 
         buttonCamara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_menuCamaraFragment);
+                navController.navigate(R.id.action_menuPerfilFragment_to_menuCamaraFragment);
             }
         });
     }
-
 }
-
-
-
-
--- app/src/main/java/com/example/playstv/MenuSuscripcionesFragment.java
+++ app/src/main/java/com/example/playstv/MenuSuscripcionesFragment.java
@@ -1,5 +1,7 @@
 package com.example.playstv;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,12 +9,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+
+
+@SuppressLint("ResourceType")
 public class MenuSuscripcionesFragment extends Fragment {
     NavController navController;
 
@@ -22,5 +28,9 @@ public class MenuSuscripcionesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_suscripciones, container, false);
     }
+
+
+
+
 
 }
